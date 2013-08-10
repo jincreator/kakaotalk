0bf11e0cdc8963b155b5293b56ad0871bbdab7f3
Taehoon Kim <carpedm20@gmail.com>
2013-08-10 20:08:27 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 __author__ = 'carpedm20'
 
 aes_key='\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-sKey = u''
-duuid = u''
-user_id = ''
+sKey = u'' # you need to know sKey for your current account
+duuid = u'' # you need to know duuid for your current account
+user_id = '' # this is not necessary for most of the commands
